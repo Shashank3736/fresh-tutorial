f16815104a7b5ff216f8a38cbf804bbff9706a4e
Shashank3736 <shreyashr267@gmail.com>
2023-03-05 12:43:44 +0530

    push

--- routes/search.tsx
+++ routes/search.tsx
@@ -23,7 +23,7 @@ export default function Page({ data }: PageProps<Data>) {
   return (
     <div>
       <form>
-        <input class="border p-2 border-4" type="text" name="q" value={query} />
+        <input className="p-4 border-4 bg-gray-200 text-gray-800 rounded shadow focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent" type="text" name="q" value={query} />
         <button type="submit">Search</button>
       </form>
       <ul>
